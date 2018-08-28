c9821ed60300efd03c5d4c3bcda4de8a62ba8999
Tomoya Horiuchi <nekopuni@gmail.com>
2018-08-28 05:30:01 +0100

    Add model stacking

--- allegro/pipeline.py
+++ allegro/pipeline.py
@@ -6,6 +6,7 @@ from sklearn.base import (BaseEstimator, TransformerMixin, RegressorMixin,
 from sklearn.externals.joblib import Parallel, delayed
 from sklearn.feature_selection import VarianceThreshold, SelectFromModel
 from sklearn.feature_selection.from_model import _get_feature_importances
+from sklearn.model_selection import KFold
 from sklearn.pipeline import FeatureUnion
 from sklearn.preprocessing import Imputer
 import xgboost as xgb
@@ -296,6 +297,7 @@ class FeatureAggregated(BaseEstimator, TransformerMixin):
 # ------------------------------------------------------------------------------
 # model
 # ------------------------------------------------------------------------------
+# cf) https://www.kaggle.com/serigne/stacked-regressions-top-4-on-leaderboard
 class ModelEnsemble(BaseEstimator, RegressorMixin, TransformerMixin):
     def __init__(self, models):
         self.models = models
@@ -311,3 +313,48 @@ class ModelEnsemble(BaseEstimator, RegressorMixin, TransformerMixin):
         predictions = np.column_stack([model.predict(X)
                                        for model in self.models])
         return np.mean(predictions, axis=1)
+
+
+class ModelStacking(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5, random_state=0):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        self.base_models_ = []
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True,
+                      random_state=self.random_state)
+
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            fold_models = []
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+                fold_models.append(instance)
+            self.base_models_.append(fold_models)
+
+        # Now train the cloned  meta-model using the out-of-fold predictions
+        # as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+    # Do the predictions of all base models on the test data and use
+    # the averaged predictions as meta-features for the final prediction
+    # which is done by the meta-model
+    def predict(self, X):
+        results = []
+        for fold_models in self.base_models_:
+            results.append(
+                np.column_stack([model.predict(X)
+                                 for model in fold_models]).mean(axis=1)
+            )
+        meta_features = np.column_stack(results)
+        return self.meta_model_.predict(meta_features)
